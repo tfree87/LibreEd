8a4955d57210d11fa0519d140871049374ebc118
Thomas Freeman <tfree87@users.noreply.github.com>
2022-01-19 21:35:55 -0500

    Created models for SIS and updated README.org diagrams

--- sis/models.py
+++ sis/models.py
@@ -2,13 +2,32 @@ from django.db import models
 
 
 class Student(models.Model):
-    student_id = models.PositiveIntegerField(primary_key=True)
-    first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50)
-    age = models.PositiveIntegerField()
-    student_email = models.EmailField()
+    """Represents data for a single student"""
+
+    id_num = models.PositiveIntegerField("Student ID Number", primary_key=True)
+    first_name = models.CharField("Student's First Name")
+    last_name = models.CharField("Student's Last Name")
+    age = models.PositiveIntegerField("Student's Age")
+    student_email = models.EmailField("Student Email Address")
+
+    def __str__(self):
+        return self.last_name + ", " + self.first_name
 
 
-class Class(models.Model):
-    class_uuid4 = models.UUIDField()
-    class_name = models.CharField(max_length=25)
+class Course(models.Model):
+    uuid4 = models.UUIDField()
+    name = models.CharField()
+    teacher = models.CharField()
+    students_in_class = models.ManyToManyField(Student, through="Enrolled")
+
+    def __str__(self):
+        return self.name
+
+
+class Enrolled(models.Model):
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    date_enrolled = models.DateField()
+
+    def __str__(self):
+        return str(date_enrolled)
