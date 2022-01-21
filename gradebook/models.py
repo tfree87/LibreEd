91801e3c12d0a09bc3673d3dd77cb63bee9263f4
Thomas Freeman <tfree87@users.noreply.github.com>
2022-01-20 21:22:50 -0500

    Added Gradebook models

--- gradebook/models.py
+++ gradebook/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Gradebook(models.Model):
+    course = models.ForeignKey("sis.Course")
+    grades = models.ManyToManyField(Grade)
+
+
+class Grade(models.Model):
+    asessement = models.ForeignKey("assessment_creator.Assessment")
+    student = models.ForeignKey("sis.Student")
+    earned_points = models.PositiveIntegerField()
+    max_points = models.PositiveIntegerField()
+    excused = models.BooleanField()
+    missing = models.BooleanField()
+    incomplete = models.BooleanField()
+    date_completed = models.DateTimeField()
+
+    def __str__(self):
+        return earned_points + "/" + max_points
